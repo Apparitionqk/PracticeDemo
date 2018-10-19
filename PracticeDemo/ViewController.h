301332a45b3ac967596b3d5c904a727122b678e1
apparitionqk@sina.cn <apparitionqk@sina.cn>
2018-10-19 10:16:46 +0800

    First commit

---
+++ PracticeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeDemo
+//
+//  Created by 齐科 on 2018/10/19.
+//  Copyright © 2018年 齐科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PracticeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PracticeDemo
+//
+//  Created by 齐科 on 2018/10/19.
+//  Copyright © 2018年 齐科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
